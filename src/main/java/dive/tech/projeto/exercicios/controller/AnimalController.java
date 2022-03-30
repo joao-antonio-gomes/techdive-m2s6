9769943ecfda399645e5a007bb4253fa055cef8f
João Gomes <devjoaoantoniogomes@gmail.com>
2022-03-29 21:13:17 -0300

    Criado endpoint listarEspecies retornando 3 especies

--- src/main/java/dive/tech/projeto/exercicios/controller/AnimalController.java
+++ src/main/java/dive/tech/projeto/exercicios/controller/AnimalController.java
@@ -1,9 +1,15 @@
 package dive.tech.projeto.exercicios.controller;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path("/animal")
 public class AnimalController {
 
-
+    @GET
+    @Produces("text/plain")
+    public String listarEspecies() {
+        return "cachorro, gato, peixe";
+    }
 }
