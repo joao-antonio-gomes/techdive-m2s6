af3993b0614ac67928d5aa88dcc3f1d68eb6e1a8
João Gomes <devjoaoantoniogomes@gmail.com>
2022-03-29 21:20:23 -0300

    Adicionado Entity Animal

---
+++ src/main/java/dive/tech/projeto/exercicios/entity/Animal.java
@@ -0,0 +1,36 @@
+package dive.tech.projeto.exercicios.entity;
+
+import java.io.Serializable;
+
+public class Animal implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String nome;
+    private String especie;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+}
