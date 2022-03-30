a6bbf594cdd17d5a3f59939599c74d4a301b8953
João Gomes <devjoaoantoniogomes@gmail.com>
2022-03-29 21:17:32 -0300

    Adicionado metodo get by id

--- src/main/java/dive/tech/projeto/exercicios/controller/AnimalController.java
+++ src/main/java/dive/tech/projeto/exercicios/controller/AnimalController.java
@@ -2,7 +2,9 @@ package dive.tech.projeto.exercicios.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Path("/animal")
 public class AnimalController {
@@ -11,5 +13,21 @@ public class AnimalController {
     @Produces("text/plain")
     public String listarEspecies() {
         return "cachorro, gato, peixe";
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response buscaranimalPorId(@PathParam("id") Long id) {
+        String animal = null;
+        if (id == 1) {
+            animal = "Macaco";
+        }
+        if (id == 2) {
+            animal = "Cachorro";
+        }
+        if (id == 3) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        return Response.ok(animal).build();
     }
 }
