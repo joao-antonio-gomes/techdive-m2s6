5b090bef11084f8eedf1e9fa76a6269ac28111cf
João Gomes <devjoaoantoniogomes@gmail.com>
2022-04-02 23:12:26 -0300

    exercicio concluido

--- src/main/java/dive/tech/projeto/exercicios/controller/CursoController.java
+++ src/main/java/dive/tech/projeto/exercicios/controller/CursoController.java
@@ -1,9 +1,17 @@
 package dive.tech.projeto.exercicios.controller;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.util.List;
 
 @Path("/curso")
 public class CursoController {
+    private List<String> cursos = List.of("Java", "Python", "C#", "C++");
 
-
+    @GET
+    @Produces("text/plain")
+    public List<String> listarCursos() {
+        return cursos;
+    }
 }
